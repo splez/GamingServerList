6ac301ead715f54176c7e6bab3e59e552a2dd0ea
splez <78141242+splez@users.noreply.github.com>
2021-08-08 23:50:45 +1200

    Add files via upload

---
+++ v3/account.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+</head>
+
+<body style="background-color:#dbd9c2">
+
+<header style="background-color:#8badb1">
+<a href="index.php"><button style="font-size: 200%;background-color: 
+#8badb1; border:none; ">GamingServerList</button></a>
+</header>
+
+<section>
+<a href="myservers.php"><button style=" background-color: 
+#b7cfbe;font-size: 200%;">MyServers</button></a>
+<a href="createServer.php"><button style=" background-color: #b7cfbe;font-size: 200%;">Add Minecraft Server Listing</button></a>
+</section>
+
+</body>
+
+</html>
